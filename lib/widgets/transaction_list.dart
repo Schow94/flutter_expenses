ca1857c500d528cb28e62808f36c86a9dcbc4ce5
Schow94 <Schow94@gmail.com>
2020-09-01 07:46:35 -0700

    Finished

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 import '../models/transaction.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -19,7 +20,7 @@ class TransactionList extends StatelessWidget {
                     'No transactions added yet',
                     style: Theme.of(context).textTheme.headline6,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                   Container(
@@ -33,42 +34,16 @@ class TransactionList extends StatelessWidget {
               );
             }),
           )
-        : ListView.builder(
-            itemBuilder: (ctx, idx) {
-              return Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: EdgeInsets.all(6),
-                      child: FittedBox(
-                        child: Text('\$${transactions[idx].amount}'),
-                      ),
-                    ),
+        : ListView(
+            children: transactions
+                .map(
+                  (tx) => TransactionItem(
+                    key: ValueKey(tx.id),
+                    transaction: tx,
+                    deleteTx: deleteTx,
                   ),
-                  title: Text(transactions[idx].title,
-                      style: Theme.of(context).textTheme.headline6),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[idx].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 360
-                      ? FlatButton.icon(
-                          icon: Icon(Icons.delete),
-                          label: Text('Delete'),
-                          textColor: Theme.of(context).errorColor,
-                          onPressed: () => deleteTx(transactions[idx].id),
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () => deleteTx(transactions[idx].id),
-                        ),
-                ),
-              );
-            },
-            itemCount: transactions.length,
+                )
+                .toList(),
           );
   }
 }
